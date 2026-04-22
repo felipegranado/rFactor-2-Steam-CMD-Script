22ed2798be929a21af6dd476a84800fd1b5d9df1
Felipe Granado <31190972+felipegranado@users.noreply.github.com>
2026-04-22 15:27:47 -0300

    Refactor SteamCMD script for clarity and efficiency

--- Script SteamCMD en.ps1
+++ Script SteamCMD en.ps1
@@ -1,17 +1,17 @@
 Set-Culture en-US
 
-#Variables
-
+# ⚙️ CONFIG — Enter the SteamCMD root folder
 $global:Root = "C:\SteamCMD"
-$global:Steam = "$Root\steamcmd.exe"
 $global:RF2 = "$Root\steamapps\common\rFactor 2 Dedicated Server"
+
+# Other variables
+$global:Steam = "$Root\steamcmd.exe"
 $global:ModMgr = "$RF2\Bin64\ModMgr.exe"
 $global:Workshop = "$Raiz\steamapps\workshop\content\365960"
 
 
 #-----
 # Keep ID content separated by comma
-
 $ContentID = Read-Host "Enter the IDs you want (separated by comma)"
 
 Write-Host "`nWhat do you want to do with the files after installation?"
@@ -23,7 +23,6 @@ $Remove = Read-Host "Choose..."
 
 #-----
 # Loop to create the Steam Download Content command
-
 $listaID = New-Object System.Collections.Generic.List[string]
 
 $ContentID = $ContentID -split "," | ForEach-Object { $_.Trim() }
@@ -35,16 +34,12 @@ $ContentID | ForEach-Object {
    $listaID.Add("+workshop_download_item 365960 $_")
 }
 
-
-#-----
 # Command to download user-informed content (using the Loop result to create a single steamcmd.exe command line)
 
 & $Steam +login anonymous $listaID +quit
 
 
-#-----
 # Loop through and install only subfolders matching the informed IDs
-
 $arquivos = Get-ChildItem $Workshop -Directory | Where-Object { $_.Name -in $ContentID } | ForEach-Object {
     Get-ChildItem $_.FullName -Recurse -File | Where-Object { $_.Extension -eq ".rfcmp" }
 }
@@ -59,7 +54,6 @@ foreach ($arquivo in $arquivos) {
 }
 
 
-#-----
 # Remove files based on user choice
 if ($Remove -eq 1) {
     $deletarOutros = Get-ChildItem $Workshop -Recurse -File | Where-Object { $_.Extension -ne ".rfcmp" -and $_.Extension -ne ".rfmod" }
@@ -67,6 +61,7 @@ if ($Remove -eq 1) {
 }
 
 if ($Remove -eq 2) {
-    $deletarTudo = Get-ChildItem $Workshop -Recurse -File
-    if ($deletarTudo.Count -gt 0) { Remove-Item $deletarTudo.FullName }
+    Get-ChildItem $Workshop -Directory | Where-Object { $_.Name -in $ContentID } | ForEach-Object {
+        Remove-Item $_.FullName -Recurse -Force
+    }
 }
